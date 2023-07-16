aca6a10dda7b008cb56f23ddf83a598afbe47a68
Joffrey Bion <joffrey.bion@gmail.com>
2023-07-16 15:07:45 +0200

    Cleanup Socket.IO error handling

--- socket-io/src/commonMain/kotlin/org/hildan/socketio/SocketIO.kt
+++ socket-io/src/commonMain/kotlin/org/hildan/socketio/SocketIO.kt
@@ -15,52 +15,75 @@ object SocketIO {
      * Binary packet types are not supported. Binary attachments are passed in subsequent web socket frames
      * (1 frame per attachment), and thus cannot be handled in this single-frame decoding function.
      */
-    fun decode(encodedData: String): SocketIOPacket {
+    fun decode(encodedData: String): SocketIOPacket = parseRawPacket(encodedData).toSocketIOPacket()
+
+    private fun parseRawPacket(encodedData: String): RawPacket {
         val match = packetFormatRegex.matchEntire(encodedData) ?: throw InvalidSocketIOPacketException(encodedData)
-        val packetType = match.groups["packetType"]?.value?.toInt()
-            ?: error("Internal error: Socket.IO format regex was matched but the packetType group is missing")
-
-        val nBinaryAttachments = match.groups["nBinaryAttachments"]?.value?.toInt() ?: 0
-        check(nBinaryAttachments == 0) { "Socket.IO packets with binary attachments are not supported" }
-
-        val namespace = match.groups["namespace"]?.value ?: "/"
-        val ackId = match.groups["ackId"]?.value?.toInt()
-        val payload = match.groups["payload"]?.value?.takeIf { it.isNotBlank() }?.let { Json.parseToJsonElement(it) }
-        return when (packetType) {
-            0 -> SocketIOPacket.Connect(namespace, payload = ensureJsonObjectOrNull(payload))
-            1 -> SocketIOPacket.Disconnect(namespace)
-            2 -> SocketIOPacket.Event(namespace, ackId, payload = ensureNonEmptyArray(payload))
-            3 -> SocketIOPacket.Ack(
-                namespace = namespace,
-                ackId = ackId ?: error("ACK packet must have an Ack ID"),
-                payload = ensureNonEmptyArray(payload)
-            )
-            4 -> SocketIOPacket.ConnectError(namespace, errorData = payload)
-            5, 6 -> error("Binary Socket.IO packets are not supported")
-            else -> error("Unknown Socket.IO packet type $packetType")
-        }
+        return RawPacket(
+            encodedData = encodedData,
+            packetType = match.groups["packetType"]?.value?.toInt()
+                ?: error("Internal error: Socket.IO format regex was matched but the packetType group is missing"),
+            nBinaryAttachments = match.groups["nBinaryAttachments"]?.value?.toInt() ?: 0,
+            namespace = match.groups["namespace"]?.value ?: "/",
+            ackId = match.groups["ackId"]?.value?.toInt(),
+            payload = match.groups["payload"]?.value?.takeIf { it.isNotBlank() }?.let { Json.parseToJsonElement(it) }
+        )
     }
 
-    private fun ensureJsonObjectOrNull(payload: JsonElement?): JsonObject? {
-        if (payload == null) {
-            return null
-        }
-        check(payload is JsonObject) { "The payload must be a JSON object" }
-        return payload
-    }
-
-    private fun ensureNonEmptyArray(payload: JsonElement?): JsonArray {
-        check(payload != null) { "The payload is mandatory" }
-        check(payload is JsonArray) { "The payload must be a JSON array" }
-        check(payload.isNotEmpty()) { "The payload must be a non-empty JSON array" }
-        return payload
+    private fun RawPacket.toSocketIOPacket(): SocketIOPacket = when (packetType) {
+        0 -> SocketIOPacket.Connect(namespace, payload = connectPayload())
+        1 -> SocketIOPacket.Disconnect(namespace)
+        2 -> SocketIOPacket.Event(namespace, ackId, payload = messagePayload())
+        3 -> SocketIOPacket.Ack(namespace, ackId = mandatoryAckId(), payload = messagePayload())
+        4 -> SocketIOPacket.ConnectError(namespace, errorData = payload)
+        5, 6 -> throw IllegalArgumentException("Binary Socket.IO packets are not supported")
+        else -> invalid("Unknown Socket.IO packet type $packetType")
     }
 }
+
+private data class RawPacket(
+    val encodedData: String,
+    val packetType: Int,
+    val nBinaryAttachments: Int,
+    val namespace: String,
+    val ackId: Int?,
+    val payload: JsonElement?,
+) {
+    init {
+        require(nBinaryAttachments == 0) { "Socket.IO packets with binary attachments are not supported" }
+    }
+}
+
+private fun RawPacket.connectPayload(): JsonObject? {
+    if (payload == null) {
+        return null
+    }
+    return payload as? JsonObject ?: invalid("The payload for CONNECT packets must be a JSON object")
+}
+
+private fun RawPacket.messagePayload(): JsonArray {
+    if (payload == null) {
+        invalid("The payload for EVENT and ACK packets is mandatory")
+    }
+    if (payload !is JsonArray) {
+        invalid("The payload for EVENT and ACK packets must be a JSON array")
+    }
+    if (payload.isEmpty()) {
+        invalid("The array payload for EVENT and ACK packets must not be empty")
+    }
+    return payload
+}
+
+private fun RawPacket.mandatoryAckId() = ackId ?: invalid("ACK packet without an Ack ID")
+
+private fun RawPacket.invalid(message: String): Nothing = throw InvalidSocketIOPacketException(encodedData, message)
 
 /**
  * An exception thrown when some encoded data doesn't represent a valid Socket.IO packet as defined by the
  * [Socket.IO protocol](https://socket.io/docs/v4/socket-io-protocol).
  */
-@Suppress("MemberVisibilityCanBePrivate", "CanBeParameter")
-class InvalidSocketIOPacketException(val encodedData: String) : Exception("Invalid Socket.IO packet: $encodedData")
-
+@Suppress("MemberVisibilityCanBePrivate")
+class InvalidSocketIOPacketException(
+    val encodedData: String,
+    message: String = "Invalid Socket.IO packet: $encodedData",
+) : Exception(message)
