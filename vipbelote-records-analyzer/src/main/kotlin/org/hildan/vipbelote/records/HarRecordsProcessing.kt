2618463ea738a5298c24cd4ff8706119784894cf
Joffrey Bion <joffrey.bion@gmail.com>
2024-01-31 02:54:39 +0100

    Refac HarRecordsProcessing.kt

--- vipbelote-records-analyzer/src/main/kotlin/org/hildan/vipbelote/records/HarRecordsProcessing.kt
+++ vipbelote-records-analyzer/src/main/kotlin/org/hildan/vipbelote/records/HarRecordsProcessing.kt
@@ -1,6 +1,5 @@
 package org.hildan.vipbelote.records
 
-import kotlinx.serialization.*
 import kotlinx.serialization.json.*
 import org.hildan.har.*
 import org.hildan.vipbelote.protocol.messages.*
@@ -11,68 +10,69 @@ import kotlin.io.path.*
 
 private val isRootProject = Path(".").absolute().normalize().name == "vipbelote"
 private val dataDir = if (isRootProject) Path("./data") else Path("../data")
-private val rawHarRecordingsDir = dataDir.resolve("har-raw").createDirectories()
-private val filteredHarRecordingsDir = dataDir.resolve("har-filtered").createDirectories()
-private val decodedDataDir = dataDir.resolve("decoded").createDirectories()
+
+private val rawHarRecordingsDir = dataDir / "har-raw"
+private val filteredHarRecordingsDir = dataDir / "har-filtered"
+private val decodedDataDir = dataDir / "decoded"
+
+private val prettyJson = Json { prettyPrint = true }
 
 fun main() {
     check(dataDir.exists()) { "The data directory $dataDir was not found" }
-    filterRawHarFiles()
-    filteredHarRecordingsDir.listDirectoryEntries().forEach(::processHarFile)
-}
-
-private fun filterRawHarFiles() {
-    val json = Json { prettyPrint = true }
-    rawHarRecordingsDir.listDirectoryEntries().forEach { harFile ->
-        println("Filtering requests with WS traffic from raw HAR file $harFile")
-        val filteredHar = harFile.parseHar().filterRequestsWithWsMessages()
-        filteredHarRecordingsDir.resolve(harFile.fileName).writeText(json.encodeToString<Har>(filteredHar))
-        harFile.deleteExisting()
+    if (rawHarRecordingsDir.exists()) {
+        rawHarRecordingsDir.listDirectoryEntries().forEach(::filterWebSocketTraffic)
     }
+    filteredHarRecordingsDir.listDirectoryEntries().forEach(::decodeHarFile)
 }
 
-private fun Har.filterRequestsWithWsMessages(): Har = copy(log = log.filterRequestsWithWsMessages())
+private fun filterWebSocketTraffic(harFile: Path) {
+    println("Filtering requests with WS traffic from raw HAR file $harFile")
+    val filteredHar = harFile.parseHar(prettyJson).filterWebSocketTraffic()
+    filteredHarRecordingsDir.resolve(harFile.name).writeHar(filteredHar, prettyJson)
+    harFile.deleteExisting()
+}
 
-private fun HarLog.filterRequestsWithWsMessages(): HarLog = copy(entries = entries.filter { it.webSocketMessages != null })
+private fun Har.filterWebSocketTraffic(): Har = copy(log = log.filterWebSocketTraffic())
+private fun HarLog.filterWebSocketTraffic(): HarLog = copy(entries = entries.filter { it.webSocketMessages != null })
 
-private fun processHarFile(harFilePath: Path) {
-    println("Processing HAR File $harFilePath...")
-    var gameState: GameState = GameState.Initial(HildanPlayerId)
+private fun decodeHarFile(harFilePath: Path) {
+    println("Decoding VIPBelote messages from HAR File $harFilePath...")
+    val outputDir = (decodedDataDir / harFilePath.nameWithoutExtension).createDirectories()
+    val statePrinter = StatePrinter(HildanPlayerId)
     harFilePath.parseHar()
         .recordsSequence()
-        .onEach {
-            if (it.message is GameMessage) {
-                val newState = gameState.updatedWith(it.message)
-                if (newState != gameState) {
-                    gameState = newState
-                    println(gameState)
-                    println("_________________________________")
-                }
-            }
-        }
+        .onEach { statePrinter.feed(it.message) }
         .groupBy { it.namespace }
         .forEach { (namespace, records) ->
             val unknownMessages = records.map { it.message }.filterIsInstance<UnknownMessage>()
             if (unknownMessages.isNotEmpty()) {
                 error("Unknown message types need to be added:\n${unknownMessages.joinToString("\n")}")
             }
-            writeRecordsToFile(harFile = harFilePath, namespace = namespace, records = records)
+            val recordsFile = outputDir / "${sanitizeForPath(namespace = namespace)}.txt"
+            recordsFile.writeRecords(records = records)
         }
 }
 
-private fun writeRecordsToFile(
-    harFile: Path,
-    namespace: String,
-    records: List<Record>,
-) {
-    val sanitizedNamespace = sanitizeForPath(namespace)
-    val decodedRecordsFile = decodedDataDir.resolve("${harFile.nameWithoutExtension}/$sanitizedNamespace.txt")
-    decodedRecordsFile.createParentDirectories()
-    val formattedRows = records.map { it.formatted() }
-    decodedRecordsFile.writeLines(formattedRows)
-}
+private class StatePrinter(selfPlayerId: String) {
+    private var state: GameState = GameState.Initial(selfPlayerId)
 
-private fun Record.formatted() = "$direction\t$namespace\t$message"
+    fun feed(message: VipBeloteMessage) {
+        if (message is GameMessage) {
+            val newState = state.updatedWith(message)
+            println(message)
+            if (newState != state) {
+                println(">>")
+                state = newState
+                println(state)
+                println("──────────────────────────────────")
+            }
+        }
+    }
+}
 
 private fun sanitizeForPath(namespace: String): String? =
     if (namespace == "/") "default" else URLEncoder.encode(namespace.drop(1), Charsets.UTF_8)
+
+private fun Path.writeRecords(records: List<Record>) {
+    writeLines(records.map { "${it.direction}\t${it.namespace}\t${it.message}" })
+}
