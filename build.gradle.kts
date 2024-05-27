69cc4ddbeede26f4d00d57c9a434d5303d050535
Joffrey Bion <joffrey.bion@gmail.com>
2024-05-27 16:10:48 +0200

    Move repositories to settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,3 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
 }
-
-allprojects {
-    repositories {
-        mavenCentral()
-    }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
 rootProject.name = "vipbelote"
 
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
 include("vipbelote-chrome-inspector")
 include("vipbelote-protocol")
 include("vipbelote-records-analyzer")
